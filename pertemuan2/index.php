dd796c1dc6329fea14a4b5f13c57b0826e6e6344
RirinSyafira <ririnsyafirak@gmail.com>
2021-02-12 13:49:03 +0700

    menyelesaikan video pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,75 @@
+<?php
+/*
+Ririn Syafira Kirana
+203040064
+https://github.com/RirinSyafira/pw2021_203040064
+Pertemuan 2 - 12 Februari 2021
+Mempelajari mengenai Sintaks PHP
+*/
+
+//Standar Output
+//echo, print: untuk mencetak tulisan, isi variabel dan lain-lain.
+//print_r: mencetak isi array
+//var_dump: untuk melihat isi informasi variabel
+
+echo "Ririn Syafira Kirana"; 
+// print "Ririn Syafira Kirana";
+// print_r ("Ririn Syafira Kirana");
+// var_dump ("Ririn Syafira Kirana");
+
+//Bisa menggunakan kutip dua maupun kutip satu
+//Menggunakan kutip dua ketika pada string terdapat penggunaan kutip satu.
+
+//Penulisan sintaks PHP
+//1. PHP di dalam HTML
+//2. HTML di dalam php
+
+//Variabel dan Tipe Data
+//Variabel
+//Tidak boleh diawali dengan angka tapi boleh mengandung angka.
+// $nama = "Ririn";
+
+//Operator
+// aritmatika : + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// penggabung string / concatenation / concat
+// .
+// $nama_depan = "Ririn";
+// $nama_belakang = "Syafira";
+// echo $nama_depan . " " . $nama_belakang
+
+//Assignment
+//=, +=, -=, *=, /=, %=, .=
+// $x = 1;
+// $x += 5;
+// echo $x;
+
+// Perbandingan
+//<, >, <=, >=, ==, !=
+// var_dump(1 < 5);
+// var_dump(1 == "1");
+
+//Identitas
+// ===, !==
+// var_dump(1 === "1")
+
+//Logika
+//&&, ||, ! 
+// $x = 10;
+// var_dump($x < 20 && $x %2 == 0);
+
+?>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Halo, Selamat Datang <?php //echo $nama; ?></h1>
+</body>
+</html> -->
