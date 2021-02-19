a00a9708a53f8689ea0819211af130ad9816eaaf
RirinSyafira <ririnsyafirak@gmail.com>
2021-02-19 14:57:44 +0700

    menyelesaikan pertemuan 3

---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Ririn Syafira Kirana
+203040064
+https://github.com/RirinSyafira/pw2021_203040064
+Pertemuan 3 - 19 Februari 2021
+Mempelajari Struktur Kendali PHP
+*/
+
+// Pengkodisian / Percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+$x = 20;
+if( $x < 20) {
+    echo "benar";
+} else if($x == 20) {
+    echo "bingo!";
+} else {
+    echo "salah";
+}
+
+?>
