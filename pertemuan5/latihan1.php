ffe49e908c077729c09bb1e3741ed6cc000112c0
RirinSyafira <ririnsyafirak@gmail.com>
2021-03-12 01:25:35 +0700

    menyelesaikan video pertemuan 5

---
+++ pertemuan5/latihan1.php
@@ -0,0 +1,31 @@
+<?php
+// Array = sebuah variabel yang dapat menampung banyak nilai
+// Elemen pada array boleh memiliki tipe data yang berbeda
+// Pasangan antara key dan value
+// Keynya adalah index, yang dimulai dari 0
+
+// Membuat Array
+// Cara Lama
+$hari = array("Senin", "Selasa", "Rabu");
+// Cara Baru
+$bulan = ["Januari", "Februari", "Maret"];
+$arr1 = [123, "tulisan", false];
+
+// Menampilkan Array
+// var_dump() / print_r()
+var_dump ($hari);
+echo "<br>";
+print_r($bulan);
+
+// Menampilakan satu elemen pada array
+echo $arr1[0];
+echo "<br>";
+echo $bulan[1];
+
+// Menampilkan elemen baru pada array
+var_dump($hari);
+$hari[] = "Kamis";
+$hari[] = "Jumat";
+echo "<br>";
+var_dump($hari);
+?>
---
+++ pertemuan5/latihan3.php
@@ -0,0 +1,35 @@
+<?php
+$mahasiswa = [
+    ["Ririn Syafira", "203040064", "Teknik Informatika", "ririnsyafirak@gmail.com"],
+    ["Siti Komalasari", "203040068", "Teknik Informatika", "sitimala@gmail.com"],
+    ["Leni Setiarini", "203040047", "Teknik Informatika", "lenisetiarini@gmail.com"]
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 3</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    
+    <?php foreach($mahasiswa as $mhs) : ?>
+    <ul>
+        <!-- <li>Ririn Syafira</li>
+        <li>203040064</li>
+        <li>Teknik Informatika</li>
+        <li>ririnsyafirak@gmail.com</li> -->
+
+        <li>Nama : <?= $mhs[0]; ?></li>
+        <li>NRP : <?= $mhs[1]; ?></li>
+        <li>Jurusan : <?= $mhs[2]; ?></li>
+        <li>Email : <?= $mhs[3]; ?></li>
+    
+    </ul>
+    <?php endforeach; ?>
+</body>
+</html>
